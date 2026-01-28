6e5986dafed38371f71ce70adf5001ff3a8e2460
rikki-shiba <r.jones@shiba-kokusai.ed.jp>
2026-01-28 13:55:14 +0900

    Updated code

---
+++ 2025-2026/Grade 4/Term 3/Lesson 04/04 Student Activity/game.js
@@ -0,0 +1,44 @@
+// Variables Setup
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ballX = canvas.width / 2;
+let ballY = canvas.height / 2;
+let radius = 40;
+
+let angle = 0;
+
+function rotateShape() {
+    // Rotate
+}
+
+function colorFadeShape() {
+    // Color Fade
+}
+
+function pulseShape() {
+    // Pulse
+}
+
+
+function gameLoop() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	angle += 0.05;
+
+	ctx.save();	// save the current state
+	ctx.translate(ballX, ballY);	// move to ball center
+	ctx.rotate(angle);	// rotate
+
+    // Draw rectangle (will be rotated)
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(-25, -25, 50, 50);
+
+	ctx.restore();	// restore state
+
+	requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
